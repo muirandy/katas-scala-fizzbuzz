00b48c4434f2ad5ad07389b3ba7728099ef51677
Andy Muir <andy.muir@aimyourtechnology.com>
2018-12-21 22:49:33 +0000

    Completed. Quite readable. Simple.

--- src/main/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzz.scala
@@ -1,8 +1,13 @@
 package com.github.muirandy.katas.scala.fizzbuzz
 
 object FizzBuzz {
-  def getResult(i: Int): String = {
-    return "fizz"
-  }
 
+  val fizz = (x: Int) => if (x % 3 == 0) "fizz" else ""
+  val buzz = (x: Int) => if (x % 5 == 0) "buzz" else ""
+  val nothing = (x: Int) => if (fizz(x).length == 0 && buzz(x).length == 0) x else ""
+
+  def getResult(i: Int): String = {
+    return fizz(i) + buzz(i) + nothing(i)
+  }
 }
+
--- src/test/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzzSpec.scala
+++ src/test/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzzSpec.scala
@@ -11,4 +11,19 @@ class FizzBuzzSpec extends FlatSpec with Matchers {
     FizzBuzz.getResult(12) should be ("fizz")
   }
 
+  it should "Buzz when a number is divisable by 5" in {
+    FizzBuzz.getResult(5) should be("buzz")
+    FizzBuzz.getResult(10) should be("buzz")
+  }
+
+  it should "FizzBuzz when a number is divisable by 3 and 5" in {
+    FizzBuzz.getResult(15) should be("fizzbuzz")
+  }
+
+  it should "return input when a number is neither divisable by 3 nor 5" in {
+    FizzBuzz.getResult(1) should be("1")
+    FizzBuzz.getResult(2) should be("2")
+    FizzBuzz.getResult(4) should be("4")
+    FizzBuzz.getResult(7) should be("7")
+  }
 }
