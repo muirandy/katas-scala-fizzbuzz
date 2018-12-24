e9eb3765e9a8bf66eea7c8ce27058805a1380f32
Andy Muir <andy.muir@aimyourtechnology.com>
2018-12-24 15:18:24 +0000

    Refactor: Removed redundant code

--- src/main/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzz.scala
@@ -1,13 +1,11 @@
 package com.github.muirandy.katas.scala.fizzbuzz
 
-import PartialFunction._
-
 object FizzBuzz {
   def getResult(i: Int): String = {
     i match {
-      case x if (i % 15 == 0) => "fizzbuzz"
-      case x if (i % 5 == 0) => "buzz"
-      case x if (i % 3 == 0) => "fizz"
+      case x if i % 15 == 0 => "fizzbuzz"
+      case x if i % 5 == 0 => "buzz"
+      case x if i % 3 == 0 => "fizz"
       case _ => "" + i
     }
   }
