6caf49af04f2b56460745c1fd619bc4e9838cb22
Andy Muir <andy.muir@aimyourtechnology.com>
2018-12-27 08:55:52 +0000

    New implementation based on function composition with 2 argument functions and currying. 3 different flavours.

--- src/main/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/github/muirandy/katas/scala/fizzbuzz/FizzBuzz.scala
@@ -1,13 +1,21 @@
 package com.github.muirandy.katas.scala.fizzbuzz
 
 object FizzBuzz {
+  private def fizz (i: Int)(result: String) = if (i % 3 == 0) result + "fizz" else result
+  private def buzz (i: Int)(result: String) = if (i % 5 == 0) result + "buzz" else result
+  private def other (i: Int)(result: String) = if (result.length == 0) "" + i else result
+
   def getResult(i: Int): String = {
-    i match {
-      case x if i % 15 == 0 => "fizzbuzz"
-      case x if i % 5 == 0 => "buzz"
-      case x if i % 3 == 0 => "fizz"
-      case _ => "" + i
-    }
+    val f = fizz(i) _
+    val b = buzz(i) _
+    val o = other(i) _
+
+    val fizzBuzz = o compose b compose f
+    //val fizzBuzz = other(i) _ compose buzz(i) _ compose(fizz(i) _)
+    //    return fizzBuzz.apply("")
+
+    val z = fizz(i) _ andThen buzz(i) _ andThen other(i) _
+    z.apply("")
   }
 }
 
